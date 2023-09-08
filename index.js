cbb16716e24fd7f799b13b780555050ac261b6d9
Yash Parwani <parwaniyash5610@gmail.com>
2023-09-08 15:58:26 +0530

    handling edgecases

--- index.js
+++ index.js
@@ -15,44 +15,42 @@ app.get('/bfhl', (req, res) => {
 
 
 app.post('/bfhl', (req, res) => {
-  const {
-    data
-  } = req.body;
+  try {
+    const data = req.body.data || [];
 
-  if (!data) {
-    return res.status(400).json({ error: 'Missing required fields in the request body' });
+    if (!data.length || !data.every(item => typeof item === 'string')) {
+      return res.status(400).json({ is_success: false, error_message: 'Invalid input data format' });
+    }
+
+    // Process the data and generate the response here
+    const alphabetsArr = data.filter((element) => (element>='A' && element <='Z') || (element>='a' && element<='z'));
+    const numsArr = data.filter((element) => !alphabetsArr.includes(element));
+     // Calculate the highest alphabet from the input array
+    const highestAlphabet = alphabetsArr.reduce((max, current) =>
+        current > max ? current : max
+    );
+    const maxAlphabet = [];
+    if(highestAlphabet){
+        maxAlphabet.push(highestAlphabet);
+    }
+
+    const response = {
+        is_success: true,
+        user_id: 'john_doe_17091999',
+        email: 'john@xyz.com',
+        roll_number: 'ABCD123',
+        numbers:numsArr,
+        alphabets:alphabetsArr,
+        highest_alphabet : maxAlphabet,
+      };
+
+    return res.status(200).json(response);
+  } catch (error) {
+    return res.status(500).json({ is_success: false, error_message: error.message });
   }
 
-  
-  const alphabetsArr = data.filter((element) => (element>='A' && element <='Z') || (element>='a' && element<='z'));
-  const numsArr = data.filter((element) => !alphabetsArr.includes(element));
-  
-    
-    
-    
-    // Calculate the highest alphabet from the input array
-  const highestAlphabet = alphabetsArr.reduce((max, current) =>
-    current > max ? current : max
-  );
-  const maxAlphabet = [];
-  if(highestAlphabet){
-    maxAlphabet.push(highestAlphabet);
-  }
-
-  const response = {
-    is_success: true,
-    user_id: 'john_doe_17091999',
-    email: 'john@xyz.com',
-    roll_number: 'ABCD123',
-    numbers:numsArr,
-    alphabets:alphabetsArr,
-    highest_alphabet : maxAlphabet,
-  };
-
-  return res.status(200).json(response);
+ 
 });
-
-
 
 app.listen(PORT, () => {
     console.log("Server running at PORT", PORT);
